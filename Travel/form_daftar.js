e18f00e0e178cc855437abd6948f15961abe70c4
Fatur Rahman <125888033+Faturrahman080402@users.noreply.github.com>
2023-05-04 21:48:44 +0700

    Delete Travel directory

--- Travel/form_daftar.js
+++
@@ -1,9 +0,0 @@
-function validatePassword() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmpassword").value;
-    if (password != confirmPassword) {
-        alert("Password tidak sesuai");
-        return false;
-    }
-    return true;
-}
